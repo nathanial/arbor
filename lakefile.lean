9ba99133a8cbb2dc8e1635bf492ffc7d7162bc21
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-31 01:08:42 -0800

    Update staple to v0.0.2

--- lakefile.lean
+++ lakefile.lean
@@ -13,7 +13,7 @@ require tincture from git "https://github.com/nathanial/tincture" @ "v0.0.1"
 
 -- Test dependencies
 require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.1"
-require staple from git "https://github.com/nathanial/staple" @ "v0.0.1"
+require staple from git "https://github.com/nathanial/staple" @ "v0.0.2"
 
 @[default_target]
 lean_lib Arbor where
